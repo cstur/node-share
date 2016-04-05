71daf5182ee51a2ca86ee7030a4c82d4aefbb5ea
Stur CheZheng <dongjie.shi@ichezheng.com>
2016-04-05 15:21:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+var myModule = require('./module');
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+
+	/* CallBack */
+	console.log(req.url);
+	fs.readFile('awesome.txt', 'utf-8', function(err, data) {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(data);
+		}
+	});
+	console.log('end.');
+
+	/* Event Emitter */
+	var event = new EventEmitter();
+	event.on('some_event', function() {
+		console.log('some_event occured.');
+	});
+	setTimeout(function() {
+		event.emit('some_event');
+	}, 3000);
+
+	/* Module */
+	myModule.setName('Stur');
+	myModule.sayHello();
+
+	res.write('<h1>NodeJS</h1>');
+	res.end('<p>Hello CheZheng</p>');
+}).listen(3000);
+console.log("HTTP server is listening at port 3000.");
